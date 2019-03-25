f0300fe3f028b3aab9472da496d762e614205929
Bryan Larson <b32147@gmail.com>
2019-03-25 09:09:11 -0400

    DBMISVC - Updated update script for cherry-picking commits

--- update.sh
+++ update.sh
@@ -3,11 +3,30 @@
 # Get the commit
 COMMIT=$1
 
-# Run through branches
-git checkout 2.7-alpine && git cherry-pick $COMMIT && git push origin 2.7-alpine
-git checkout 2.7-slim && git cherry-pick $COMMIT && git push origin 2.7-slim
-git checkout 3.6-alpine && git cherry-pick $COMMIT && git push origin 3.6-alpine
-git checkout 3.6-slim && git cherry-pick $COMMIT && git push origin 3.6-slim
-git checkout 3.7-alpine && git cherry-pick $COMMIT && git push origin 3.7-alpine
-git checkout 3.7-slim && git cherry-pick $COMMIT && git push origin 3.7-slim
-git checkout master && git cherry-pick $COMMIT && git push origin master
+# Check to push
+PUSH_TO_ORIGIN=$2
+
+# Iterate through each branch
+for branch in $(git for-each-ref --format='%(refname:short)' refs/heads/); do
+
+    # Prompt for cherry-picking
+    read -p "Apply commit to $branch? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        # Apply the commit
+        echo -e "\nApplying $COMMIT to $branch...\n"
+        git checkout "$branch" && git cherry-pick "$COMMIT"
+
+        # Prompt for pushing
+        read -p "Push $branch to origin? " -n 1 -r
+        echo    # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            # Push to origin
+            echo -e "\nPushing $branch to origin...\n"
+            git push origin "$branch"
+        fi
+    fi
+
+done
